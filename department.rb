e3aa267cc5e7c5935267f97b7253d8a335c60979
Molly G <gehring.mg@gmail.com>
2016-02-11 15:32:12 -0500

    Add method for determining total salaries in a department

--- department.rb
+++ department.rb
@@ -9,4 +9,13 @@ class Department
   def add_employee(new_employee)
     @employees << new_employee
   end
+
+  def get_total_salaries(department)
+    d = department
+    salaries = 0
+    d.employees.each do |i|
+      salaries = salaries + i.salary
+    end
+    return salaries
+  end
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -28,6 +28,18 @@ class EmployeeReviewsTest < Minitest::Test
     d = Department.new("Marketing")
     e = Employee.new("John Smith", "Johnsmith@aol.com", "555-5555", 100000)
     d.add_employee(e)
-    assert_equal e, employees[0]
+    assert_equal e, d.employees[0]
+  end
+
+  def test_get_total_department_salaries
+    d = Department.new("Marketing")
+    e1 = Employee.new("John Doe", "Johnsmith@aol.com", "555-5555", 100000)
+    e2 = Employee.new("Joe Smith", "Johnsmith@aol.com", "555-5555", 55000)
+    e3 = Employee.new("Jane Smith", "Johnsmith@aol.com", "555-5555", 500000)
+    d.add_employee(e1)
+    d.add_employee(e2)
+    d.add_employee(e3)
+    bigmoney = d.get_total_salaries(d)
+    assert_equal 655000, bigmoney
   end
 end
